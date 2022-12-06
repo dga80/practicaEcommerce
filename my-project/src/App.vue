8670e73b5071a510ebb4f3b368a8814c5d37fa0e
dga80 <dani.dga@gmail.com>
2022-12-06 17:31:35 +0100

    funcional

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -5,19 +5,51 @@ export default {
     return {
       items: LIST,
     }
+  },
+  computed:{
+    result () {
+      return this.item===active
+    }
   }
 }
 </script>
 
 <template>
-  <div>
-    <article v-for="item of items" class="flex">
-      <p>{{item.name}}</p>
-      <img :src="item.image" :alt="item.name" class="w-40">
-
-    </article>
-  </div>
+  
+  <article class="flex justify-around">
+    <div>
+      <ul v-for="item of items">
+        <li class="pb-20" v-if="active"></li>
+      </ul>
+    </div>
+    <div>
+      <ul v-for="item of items">
+        <li class="pb-10"><img :src="item.image" :alt="item.name" class="w-20"></li>
+      </ul>
+    </div>
+    <div>
+      <ul v-for="item of items">
+        <li class="pb-20">{{item.price}}</li>
+      </ul>
+    </div>
+    <div>
+      <ul v-for="item of items">
+        <li class="pb-20">
+          <input type="number" v-model="item.quantity" class="text-right border-2 border-stone-900"/>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul v-for="item of items">
+          <li class="pb-20">{{item.price * item.quantity}}</li>
+      </ul>
+    </div>
+  </article>
+  
 </template>
 
 <style scoped>
+  .articleSize{
+    height: 100vh;
+    }
 </style>
--- my-project/tailwind.config.cjs
+++ my-project/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      outlineWidth: {
+        1: '1px',
+      }
+    },
   },
   plugins: [],
 }
