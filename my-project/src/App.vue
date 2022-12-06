afe121eb132396dc7a736207ac37789bd5ab2bcb
dga80 <dani.dga@gmail.com>
2022-12-06 16:19:32 +0100

    firs results

---
+++ my-project/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import LIST from "./items.json";
+export default {
+  data () {
+    return {
+      items: LIST,
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <article v-for="item of items" class="flex">
+      <p>{{item.name}}</p>
+      <img :src="item.image" :alt="item.name" class="w-40">
+
+    </article>
+  </div>
+</template>
+
+<style scoped>
+</style>
